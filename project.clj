ddea56554981cb28936288fc4f1916b35a789697
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-05-02 10:38:14 +0200

    Updates project clj with new app namespace

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [cheshire "5.3.1"]
                  [com.taoensso/timbre "3.1.6"]]
   :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler uma.handler/app}
+  :ring {:handler uma.server/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
