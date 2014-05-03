b3c082c006e70020a47a87316dcf8278385a8f07
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-05-03 17:12:49 +0200

    Adds crud macro

--- src/uma/routes/users.clj
+++ src/uma/routes/users.clj
@@ -1,32 +1,10 @@
 (ns uma.routes.users
-  (require [compojure.core :refer [defroutes context GET POST DELETE]]
-           [liberator.core :refer [resource]]
-           [uma.models.users :as users]
-           [uma.utilities :refer [resource-defaults]]))
+  (require [uma.models.users :as users]
+           [uma.utilities :refer [defroutes-with-crud]]))
 
-(defroutes routes
-  (GET "/" []
-    (resource resource-defaults
-      :handle-ok (users/get-users)))
-  (POST "/" {:keys [params]}
-    (resource resource-defaults
-      :allowed-methods [:post]
-      :post! {::user (users/create-user params)}
-      :handle-created ::user))
-  (GET "/:id" [id]
-    (resource resource-defaults
-      :handle-ok (let [as-integer (Integer. id)]
-                   (users/get-user-by-id as-integer))))
-  (POST "/:id" {:keys [params]}
-    (resource resource-defaults
-      :allowed-methods [:post]
-      :respond-with-entity true
-      :post! (let [as-integer (Integer. (:id params))
-                   mapped (dissoc params :id)]
-               {::user (users/update-user as-integer mapped)})
-      :handle-created ::user))
-  (DELETE "/:id" [id]
-    (resource resource-defaults
-      :allowed-methods [:delete]
-      :delete! (let [as-integer (Integer. id)]
-                 (users/delete-user as-integer)))))
+(defroutes-with-crud routes
+  {:read-all users/get-users
+   :read-by-id users/get-user-by-id
+   :create users/create-user
+   :update users/update-user
+   :delete users/delete-user})
--- src/uma/utilities.clj
+++ src/uma/utilities.clj
@@ -1,6 +1,6 @@
 (ns uma.utilities
   (require [cheshire.core :refer [generate-string]]
-           [compojure.core :refer [routes]]
+           [compojure.core :refer [routes defroutes GET POST DELETE]]
            [liberator.core :refer [resource]]))
 
 (defn json-response
@@ -18,3 +18,33 @@
 
 (def resource-defaults
   {:available-media-types ["application/json"]})
+
+(defmacro defroutes-with-crud
+  [sym {:keys [create read-all read-by-id update delete]} & body]
+  `(defroutes ~sym
+    (GET "/" []
+      (resource resource-defaults
+        :handle-ok (~read-all)))
+    (POST "/" {:keys [~'params]}
+      (resource resource-defaults
+        :allowed-methods [:post]
+        :post! {::user (~create ~'params)}
+        :handle-created ::user))
+    (GET "/:id" [~'id]
+      (resource resource-defaults
+        :handle-ok (let [~'as-integer (Integer. ~'id)]
+                     (~read-by-id ~'as-integer))))
+    (POST "/:id" {:keys [~'params]}
+      (resource resource-defaults
+        :allowed-methods [:post]
+        :respond-with-entity true
+        :post! (let [~'as-integer (Integer. (:id ~'params))
+                     ~'mapped (dissoc ~'params :id)]
+                 {::user (~update ~'as-integer ~'mapped)})
+        :handle-created ::user))
+    (DELETE "/:id" [~'id]
+      (resource resource-defaults
+        :allowed-methods [:delete]
+        :delete! (let [~'as-integer (Integer. ~'id)]
+                   (~delete ~'as-integer))))
+    ~@body))
