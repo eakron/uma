fe90ee58f31d21e8033ede5eabea4f598b9e1ce0
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-05-03 15:25:19 +0200

    Collapses user route structure

--- src/uma/routes/users.clj
+++ src/uma/routes/users.clj
@@ -1,37 +1,32 @@
 (ns uma.routes.users
   (require [compojure.core :refer [defroutes context GET POST DELETE]]
-           [liberator.core :refer [defresource]]
+           [liberator.core :refer [resource]]
            [uma.models.users :as users]
            [uma.utilities :refer [resource-defaults]]))
 
-(defresource get-resource resource-defaults
-  :handle-ok (users/get-users))
-
-(defresource create-resource [user] resource-defaults
-  :allowed-methods [:post]
-  :post! {::user (users/create-user user)}
-  :handle-created ::user)
-
-(defresource get-resource-by-id [id] resource-defaults
-  :handle-ok (let [as-integer (Integer. id)]
-               (users/get-user-by-id as-integer)))
-
-(defresource update-resource [{:keys [id] :as user}] resource-defaults
-  :allowed-methods [:post]
-  :respond-with-entity true
-  :post! (let [as-integer (Integer. id)
-               mapped (dissoc user :id)]
-           {::user (users/update-user as-integer mapped)})
-  :handle-created ::user)
-
-(defresource delete-resource [id] resource-defaults
-  :allowed-methods [:delete]
-  :delete! (let [as-integer (Integer. id)]
-             (users/delete-user as-integer)))
-
 (defroutes routes
-  (GET "/" request get-resource)
-  (POST "/" {:keys [params]} (create-resource params))
-  (GET "/:id" [id] (get-resource-by-id id))
-  (POST "/:id" {:keys [params]} (update-resource params))
-  (DELETE "/:id" [id] (delete-resource id)))
+  (GET "/" []
+    (resource resource-defaults
+      :handle-ok (users/get-users)))
+  (POST "/" {:keys [params]}
+    (resource resource-defaults
+      :allowed-methods [:post]
+      :post! {::user (users/create-user params)}
+      :handle-created ::user))
+  (GET "/:id" [id]
+    (resource resource-defaults
+      :handle-ok (let [as-integer (Integer. id)]
+                   (users/get-user-by-id as-integer))))
+  (POST "/:id" {:keys [params]}
+    (resource resource-defaults
+      :allowed-methods [:post]
+      :respond-with-entity true
+      :post! (let [as-integer (Integer. (:id params))
+                   mapped (dissoc params :id)]
+               {::user (users/update-user as-integer mapped)})
+      :handle-created ::user))
+  (DELETE "/:id" [id]
+    (resource resource-defaults
+      :allowed-methods [:delete]
+      :delete! (let [as-integer (Integer. id)]
+                 (users/delete-user as-integer)))))
