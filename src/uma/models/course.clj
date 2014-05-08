5d4eea4639e898915402b4fd4477d3e16b1fb5af
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-05-08 21:45:02 +0200

    Adds get-registered-users by course id
    Resolves #2

--- src/uma/models/course.clj
+++ src/uma/models/course.clj
@@ -1,6 +1,6 @@
 (ns uma.models.course
   (require [uma.database :as db]
-           [uma.models.entities :refer [occasion user course]]
+           [uma.models.entities :refer [user course registered]]
            [korma.core :refer :all]))
 
 (defn get-courses []
@@ -22,3 +22,8 @@
 (defn delete-course [id]
   (delete course
     (where {:id id})))
+
+(defn get-registered-users [id]
+  (select user
+    (join registered (= :registered.uma_user_id :id))
+    (where {:registered.course_id id})))
--- src/uma/routes/course.clj
+++ src/uma/routes/course.clj
@@ -1,10 +1,16 @@
 (ns uma.routes.course
   (require [uma.models.course :as course]
-           [uma.utilities :refer [defroutes-with-crud]]))
+           [uma.utilities :refer [defroutes-with-crud resource-defaults]]
+           [liberator.core :refer [resource]]
+           [compojure.core :refer [GET]]))
 
 (defroutes-with-crud routes
   {:read-all course/get-courses
    :read-by-id course/get-course-by-id
    :create course/create-course
    :update course/update-course
-   :delete course/delete-course})
+   :delete course/delete-course}
+  (GET "/:id/users" [id]
+    (resource resource-defaults
+      :handle-ok (let [as-integer (Integer. id)]
+                   (course/get-registered-users as-integer)))))
