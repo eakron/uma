969cf5ffda6fc58dabea883424d421cced01e697
Erik Kronberg <erik.a.kronberg@gmail.com>
2014-05-08 23:13:41 +0200

    Fixes json encoding of java.sql.Time

--- src/uma/database.clj
+++ src/uma/database.clj
@@ -1,9 +1,16 @@
 (ns uma.database
   (require [environ.core :refer [env]]
-           [korma.db :refer [defdb postgres]]))
+           [clojure.data.json :as json]
+           [korma.db :refer [defdb postgres]])
+  (import [java.sql Time]))
 
 (defdb db (postgres {:db (env :database-name)
                      :user (env :database-user)
                      :password (env :database-password)
                      :host (env :database-host)
                      :port "5432"}))
+
+(extend-type java.sql.Time
+  json/JSONWriter
+  (-write [date out]
+    (json/-write (str date) out)))
